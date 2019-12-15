76e2ddee48f262bcc6bc58a57b1122036ae48d7a
Aldo Huayhua <aldohuayhua@gmail.com>
2019-12-14 20:21:44 -0500

    adding env variable connection to myqsl so it can launch in heroku

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,9 @@ var mysql = require("mysql");
 
 var connection;
 
-
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
 connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,7 +13,7 @@ connection = mysql.createConnection({
     password: "password",
     database: "burger_db"
 });
-
+};
 // Make connection.
 connection.connect(function (err) {
     if (err) {
